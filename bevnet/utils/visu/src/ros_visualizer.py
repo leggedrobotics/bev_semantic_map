0a5407d54acfe391336a2a6b34781c967b4f94d9
schmirob <schmirob@student.ethz.ch>
2023-08-11 16:28:22 +0200

    Visualize occupancy gridmap

--- bevnet/utils/visu/src/ros_visualizer.py
+++ bevnet/utils/visu/src/ros_visualizer.py
@@ -1,12 +1,14 @@
 import rospy
 from grid_map_msgs.msg import GridMap, GridMapInfo
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 from std_msgs.msg import Float32MultiArray, MultiArrayDimension
 import numpy as np
 
-class NumpyToGridmapVisualizer:
+class NumpyToMapVisualizer:
     def __init__(self, init_node=True):
         rospy.init_node("np_to_gridmap_visualizer", anonymous=False)
-        self.pub_gridmap = rospy.Publisher("gridmap", GridMap, queue_size=1)
+        self.pub_grid_map = rospy.Publisher("gridmap", GridMap, queue_size=1)
+        self.pub_occupancy_map = rospy.Publisher("occupancy_grid", OccupancyGrid, queue_size=1)
 
     def gridmap_arr(self, arr, res, layers, reference_frame="map", publish=True, x=0, y=0):
         size_x = arr.shape[1]
@@ -42,23 +44,50 @@ class NumpyToGridmapVisualizer:
         gm_msg = GridMap(info=info, layers=layers, basic_layers=[], data=data)
         if publish:
             print("publishing")
-            self.pub_gridmap.publish(gm_msg)
+            self.pub_grid_map.publish(gm_msg)
         return gm_msg
+
+    def occupancy_map_arr(self, arr, res, reference_frame="map", publish=True, x=0, y=0):
+        size_x = arr.shape[1]
+        size_y = arr.shape[2]
+
+        center_offset_x = (size_x * res) / 2
+        center_offset_y = (size_y * res) / 2
+
+        occupancy_grid = OccupancyGrid()
+        occupancy_grid.header.seq = 0
+        occupancy_grid.header.stamp = rospy.Time.now()
+        occupancy_grid.header.frame_id = reference_frame
+        occupancy_grid.info.resolution = res
+        occupancy_grid.info.width = size_x
+        occupancy_grid.info.height = size_y
+        occupancy_grid.info.origin.position.x = x - center_offset_x  # Adjusted x origin
+        occupancy_grid.info.origin.position.y = y - center_offset_y  # Adjusted y origin
+        occupancy_grid.data = (arr * 100).astype(np.int8).ravel()  # Scale values to 0-100 and flatten
+
+        if publish:
+            print("publishing")
+            self.pub_occupancy_map.publish(occupancy_grid)
+        return occupancy_grid
 
 
 if __name__ == "__main__":
-    vis = NumpyToGridmapVisualizer()
+    vis = NumpyToMapVisualizer()
 
     shape = (1, 128, 128)
-    arr = np.random.rand(*shape) * 3
+    arr = np.random.rand(*shape)
 
     # Threshold arr if bigger than 1
-    arr[arr > 1] = 1
-    arr[arr <= 0] = 0
+    arr[arr > 0.5] = 1
+    arr[arr <= 0.5] = 0
 
     res = 0.1
     layers = ["l1"]
+    #
+    # while not rospy.is_shutdown():
+    #     vis.gridmap_arr(arr, res, layers, x=0, y=0)
+    #     rospy.sleep(1)
 
     while not rospy.is_shutdown():
-        vis.gridmap_arr(arr, res, layers, x=0, y=0)
+        vis.occupancy_map_arr(arr, res, x=0, y=0)
         rospy.sleep(1)
