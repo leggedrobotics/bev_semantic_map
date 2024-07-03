37dc19e23af975782458ba974fd84891372680fc
RobinSchmid7 <schmirob@student.ethz.ch>
2024-07-03 15:17:08 +0200

    Moved files

--- bev_semantic_map_ros/setup.py
+++ bev_semantic_map_ros/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["perception_bev_learning_ros"],
+    packages=["bev_semantic_map_ros"],
     package_dir={"": "src"},
 )
 
